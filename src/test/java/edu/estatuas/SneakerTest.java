b53d5ff5c98540a4a5aa7242083cc71e41bff2ff
Rubi Quintas Alonso <untalrubi@gmail.com>
2025-03-21 13:33:25 +0100

    test(SneakerTest): Create tests for Sneaker
    Added get, set and toString tests to class Sneaker.

--- src/test/java/edu/estatuas/SneakerTest.java
+++ src/test/java/edu/estatuas/SneakerTest.java
@@ -1,13 +1,44 @@
 package edu.estatuas;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import edu.estatuas.stockx.item.Sneaker;
 
 public class SneakerTest 
 {
+    private Sneaker sneaker;
+
+    @BeforeEach
+    public void createSneaker(){
+        sneaker = new Sneaker("123-ABC", "Zapas");
+    }
 
     @Test
-    public void shouldAnswerWithTrue()
+    public void testSetGet()
     {
+        assertEquals("123-ABC", sneaker.getStyle());
+        assertEquals("Zapas", sneaker.getName());
+
+        assertEquals(0, sneaker.getSale());
+        sneaker.setSale(150);
+        assertEquals(150, sneaker.getSale());
+
+        assertEquals(0, sneaker.getAsk());
+        sneaker.setAsk(25);
+        assertEquals(25,sneaker.getAsk());
         
+        assertEquals(0, sneaker.getBid());
+        sneaker.setBid(40);
+        assertEquals(40, sneaker.getBid());
+    }
+
+
+    @Test
+    public void testToString(){
+        assertEquals("Zapas\n\t\t123-ABC", sneaker.toString());
     }
 }
