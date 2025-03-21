0e669df6249f0c04f48254f9aa9e38e9a003021c
Rubi Quintas Alonso <untalrubi@gmail.com>
2025-03-21 12:53:55 +0100

    ci(gitignore): Add gitignore

--- src/main/java/edu/estatuas/stockx/Stockx.java
+++ src/main/java/edu/estatuas/stockx/Stockx.java
@@ -42,12 +42,14 @@ public class Stockx {
          * en sus offers.
          */
 
+        /*
         Bid bid = new Bid("13", 550);
         sneaker.add(bid);
         sneaker.add(new Bid("6", 200));
         sneaker.add(new Bid("9.5", 479));
         sneaker.add(new Bid("13", 338));
         sneaker.add(new Bid("9.5", 480));
+         */
     }
 
     public static String draw(Item sneaker) {
---
+++ src/test/java/edu/estatuas/SneakerTest.java
@@ -0,0 +1,13 @@
+package edu.estatuas;
+
+import org.junit.Test;
+
+public class SneakerTest 
+{
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        
+    }
+}
